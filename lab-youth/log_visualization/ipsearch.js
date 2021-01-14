4a5f4a7b91ccc995dd90c3ee95d49fbda6ed2333
nonnonno <ayatyan.2525@gmail.com>
2021-01-14 16:42:09 +0900

    configure geoip-test-js

--- lab-youth/log_visualization/ipsearch.js
+++ lab-youth/log_visualization/ipsearch.js
@@ -5,11 +5,15 @@
 const geoip = require('/Users/ayano/lab-youth/log_visualization/node_modules/geoip-country/lib/geoip');
 const fs = require('fs');
 
-let buff = fs.readFileSync("./ip.txt", {encoding: "utf-8"}).split(/\r\n/);
+let buff = fs.readFileSync("./ip.txt", {encoding: "utf-8"}).split(/\n/);
+console.log(buff);
+console.log(buff.length);
 for(let i=0; i<buff.length; i++){
+  console.log(i);
   let [count,ip] = buff[i].split(/\t/);
+  //console.log("count:"+count);
   let geo = geoip.lookup(ip);
-  let country = ( (geo!== null) && ("country" in geo) )?  geo.country:null
+  let country = ( (geo!== null) && ("country" in geo) )?  geo.country:null;
   console.log(country);
 
   console.log([count, ip, country].join("\t"));
--- lab-youth/log_visualization/simple.js
+++ lab-youth/log_visualization/simple.js
@@ -1,5 +1,6 @@
 const geoip = require('/Users/ayano/lab-youth/log_visualization/node_modules/geoip-country/lib/geoip');
 let country = geoip.lookup('133.65.65.35');
 let range = geoip.pretty(country.range);
+//change ip address range format
 console.log(country);
 console.log(range);
