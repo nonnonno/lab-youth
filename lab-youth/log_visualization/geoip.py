1fcdb62a88d36ad81ff957ebbeea31f0ed3bfb7f
nonnonno <ayatyan.2525@gmail.com>
2021-02-04 13:31:34 +0900

    extract only lati&longi

--- lab-youth/log_visualization/geoip.py
+++ lab-youth/log_visualization/geoip.py
@@ -3,10 +3,7 @@ from geolite2 import geolite2
 geo_r= geolite2.reader()
 res = geo_r.get("2.2.2.2")
 for key,value in res.items():
-  print(key)
-  for key2,value2 in value.items():
-    if key2 == "names":
-      for n_key,n_value in value2.items():
-        print(" - {}: {}".format(n_key,n_value.encode('utf-8')))
-    else:   
-      print(" - {}: {}".format(key2,value2))
+  # print(key)
+  for key2,value2 in value.items():  
+      if "latitude" in key2 or "longitude" in key2:
+        print("{}: {}".format(key2,value2))
