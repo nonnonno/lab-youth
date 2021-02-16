8b15023ebcc04dc1539f63748611cec69ddd2b8f
nonnonno <ayatyan.2525@gmail.com>
2021-02-16 17:03:56 +0900

    extract lati&longi including zero

--- lab-youth/log_visualization/geoip.py
+++ lab-youth/log_visualization/geoip.py
@@ -1,9 +1,49 @@
 from geolite2 import geolite2
- 
+import sys
+# IPから緯度経度検索プログラム
+import ip_to_lati_longi
+
+# shell scriptによって並び替えられたsecureデータからIPを配列に格納
+ip=[]
+ip_splited=[]
+fp = open('./securelog/ip_secure_sorted.txt', 'r')
+for line in fp.readlines():
+  line = line.strip() #前後空白削除
+  # line = line.replace('\n','') #末尾の\nの削除
+  # line.split()
+  line = line.split(",") #分割
+  ip.append(line)
+fp.close()
+# これで出現回数とIPがipの配列に入った
+# print(ip)
+# print(ip[1][0])
+
+# カンマ区切りの配列を、区切り文字でsplitして配列に格納
+add=0
+for add in range(len(ip)):
+  # print(ip[add][0])
+  ip_splited.append(ip[add][0].split())
+
+ip_add=0
+# これでIPだけ取れる
+# print(ip_splited[4][1])
+
+# IPから緯度経度情報
+# geo_r= geolite2.reader()
+# res = geo_r.get(ip_splited[4][1])
 geo_r= geolite2.reader()
-res = geo_r.get("2.2.2.2")
-for key,value in res.items():
-  # print(key)
-  for key2,value2 in value.items():  
-      if "latitude" in key2 or "longitude" in key2:
-        print("{}: {}".format(key2,value2))
+
+# IPの分だけ緯度経度情報が欲しい
+# lati_longは緯度経度情報リスト
+lati_long=[]
+for ip_add in range(len(ip_splited)):
+  # res = geo_r.get(ip_splited[ip_add][1])
+  # ip_addはIPアドレスの個数
+  # 空の配列はスキップ
+  if not len(ip_splited[ip_add])==2:
+    ip_add+=1
+  lati_long.append(ip_to_lati_longi.ip_convert(ip_splited[ip_add][1]))
+  print("finish data process")
+  ip_add+=1
+print(lati_long)
+
