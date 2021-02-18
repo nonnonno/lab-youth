7fa8f58afeda2d25acf728f58640e7a68f7ae4c9
nonnonno <ayatyan.2525@gmail.com>
2021-02-18 10:42:38 +0900

    remove more print

--- lab-youth/log_visualization/ip_to_lati_longi.py
+++ lab-youth/log_visualization/ip_to_lati_longi.py
@@ -1,28 +1,20 @@
 from geolite2 import geolite2
-# import sys
 
 def ip_convert(ip):
-  # print(ip)
   geo_r = geolite2.reader()
   res = geo_r.get(ip)
   # pairは緯度経度のペアのリスト
   pair = []
   if res is not None:
     for key,value in res.items():
-      
-      # print(ip)
       # keyはcity continent coutnry locationとか
-      # print(key)
       # valueはgeonameidとか複数の国籍における国名
       for key2,value2 in value.items():  
         if "latitude" in key2 or "longitude" in key2:
           # print("{}: {}".format(key2,value2))
           pair.append(value2)
           if pair and len(pair)>=2:
-            return pair
-  # else:
-  #   return 
-        
+            return pair     
 
 if __name__ == '__main__':
   ip_convert("193.239.147.226")
