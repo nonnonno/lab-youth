4277537f5e9ad2fd78b42f5999e7bf9a89efb9cb
nonnonno <ayatyan.2525@gmail.com>
2021-03-04 11:50:03 +0900

    choose file

--- lab-youth/log_visualization/return_data.js
+++ lab-youth/log_visualization/return_data.js
@@ -1,11 +1,28 @@
 const URL = 'http://localhost:8000/'
 
+var inputFiles = document.getElementById('inputfiles');
+inputFiles.addEventListener("change", function(e) {
+ console.log(e.target.files)
+},false);
+
+inputFiles.addEventListener("change", function(e) {
+
+var file = e.target.files 
+var reader = new FileReader()
+reader.readAsDataURL(file[0])
+// reader.readAsText('./secure.txt')
+reader.onload = function() {
+console.log("file read")
+}
+}, false)
+
 ///secure.txtのファイルを読み込ませて、index.htmlに渡したい
 const OnButtonClick = () =>{
-    console.log("button was pushed");
-    readTextFile('./secure.txt');
+    console.log("button was pushed")
     
+    // readTextFile('./secure.txt')
 }
+
 function readTextFile(file)
 {
     var rawFile = new XMLHttpRequest();
