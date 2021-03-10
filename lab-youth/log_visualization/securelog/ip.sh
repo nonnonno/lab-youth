50c5fe7a5d6b1e4779b8d8d1b923e144d63fd3fe
nonnonno <ayatyan.2525@gmail.com>
2021-03-10 12:01:50 +0900

    remove null

--- lab-youth/log_visualization/securelog/ip.sh
+++ lab-youth/log_visualization/securelog/ip.sh
@@ -6,4 +6,8 @@ if [ $# != 1 ]; then
 fi
 grep Invalid $1 | sed -n '/Invalid user/p' | awk '{print $10}'|sed -e 's/[^0-9+.]//g' >> ip_$1.txt
 
-sort ip_$1.txt| uniq -c | sort -r -n >> ip_$1_sorted.txt
+sed  '/^$/d' ip_$1.txt >> ip_$1_nullremoved.txt 
+
+sort ip_$1_nullremoved.txt | uniq -c | sort -r -n >> ip_$1_sorted.txt
+
+
